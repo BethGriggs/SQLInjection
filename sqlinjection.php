247d5e4643a10569fa8b5ba3e24d18d5b56d9029
Bethany Nicolle Griggs <bethanygriggs@outlook.com>
2015-11-25 14:51:30 +0000

    Updated challenge

--- sqlinjection.php
+++ sqlinjection.php
@@ -1,67 +1,70 @@
 <html>
-<form>
-    Username:
-    <input type="text" name="username" size="15" />
-    <br /> Password:
-    <input type="password" name="password" size="15" />
-    <br />
-    <p>
-        <input type="submit" value="Login" />
-    </p>
-</form>
-
 <h1>Challenge</h1>
-<p>Your challenge is to find out the total number of users by injecting SQL into the 'Username' field above.</p>
+<p>Your challenge is to find out the total number of users by injecting SQL into the 'Username' field below.</p>
+
+<form>
+    <label for="username">Username:</label>
+    <input type="text" name="username" size="25" placeholder="inject your SQL here!" />
+    <input type="submit" value="Submit" />
+</form>
 
 <?php
 
-## Default MySQL
-mysql_connect("localhost", "root", "");
+$host   = "localhost";
+$dbUser = "root";
+$dbPass = "";
+$dbName = "demo";
 
+try {
+    
+    // MySQL setup
+    $pdo  = new PDO("mysql:host=$host", $dbUser, $dbPass);
+    $stmt = $pdo->prepare("CREATE DATABASE IF NOT EXISTS $dbName");
+    $stmt->execute();
+    $pdo = new PDO("mysql:host=$host;dbname=$dbName", $dbUser, $dbPass);
+    
+    
+    $userTable = "CREATE TABLE IF NOT EXISTS `users` (
+ `username` varchar(32) NOT NULL default '',
+ `password` varchar(32) default NULL,
+ PRIMARY KEY  (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+    
+    
+    $pdo->exec($userTable);
+    $q        = $pdo->query("SELECT * FROM users;");
+    $rowCount = $q->rowCount();
+    
+    if ($rowCount == 0) {
+        $pdo->exec("INSERT INTO `users` VALUES ('Joe', 'Bloggs')");
+        $pdo->exec("INSERT INTO `users` VALUES ('Fred', 'Bloggs')");
+        $pdo->exec("INSERT INTO `users` VALUES ('Admin', 'User');");
+    }
+}
+catch (PDOException $e) {
+    echo $e->getMessage();
+}
 
-## Database/Table Setup
-mysql_query("CREATE DATABASE IF NOT EXISTS demo;");            
-mysql_select_db("demo");
+// Set GBK character set
+$pdo->query('SET NAMES gbk');
 
-mysql_query("CREATE TABLE IF NOT EXISTS `users` (
-  `username` varchar(32) NOT NULL default '',
-  `password` varchar(32) default NULL,
-  PRIMARY KEY  (`username`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
-mysql_query("INSERT INTO `users` VALUES ('Joe', 'Bloggs')");
-mysql_query("INSERT INTO `users` VALUES ('Fred', 'Bloggs')");
-mysql_query("INSERT INTO `users` VALUES ('Admin', 'User');");
+// PDO prepared statement
+$stmt = $pdo->prepare('SELECT * FROM users WHERE username = ? LIMIT 1');
+$stmt->execute(array(
+    $_GET['username']
+));
 
-## Set GBK Character Set
-mysql_query("SET NAMES GBK");
+$result = $stmt->fetchAll();
+echo "Result: " . json_encode($result);
 
-#$_GET['username'] = chr(0xbf).chr(0x27).' OR username = username /*';
+$q        = $pdo->query("SELECT * FROM users;");
+$qResult = $q->fetchAll();
 
-## addslashes() example
+if ($result == $qResult){
+    echo "<h1 style='color: green;'>Challenge Completed!</h1>";
+}
 
-echo "<h2>1. addslashes() </h2>";
-$username = addslashes($_GET['username']);
-$password = addslashes($_GET['password']);
-$sql = "SELECT * FROM  users WHERE  username = '$username' AND password = '$password'";
-$result = mysql_query($sql) or trigger_error(mysql_error().$sql);
-
-echo("Username:  ". $username ."</br>");
-echo("Number of Rows: ". mysql_num_rows($result). "</br>");
-echo("Client Encoding: ". mysql_client_encoding(). "</br>");
-
-## mysql_real_escape_string() 
-echo "<h2>2. mysql_real_escape_string()</h2>";
-$username = mysql_real_escape_string($_GET['username']);
-$password = mysql_real_escape_string($_GET['password']);
-$sql = "SELECT * FROM  users WHERE  username = '$username' AND password = '$password'";
-$result = mysql_query($sql) or trigger_error(mysql_error().$sql);
-
-echo ("Username: ".$username. "</br>");
-echo ("Number of Rows: " .mysql_num_rows($result)."</br>");
-echo ("Client Encoding: " .mysql_client_encoding(). "</br>");
-
-echo "</br></br>PHP version: ".PHP_VERSION."</br>";
-
+echo "</br></br><p>PHP version: " . PHP_VERSION . "</p></br>";
 ?>
 
 </html>
